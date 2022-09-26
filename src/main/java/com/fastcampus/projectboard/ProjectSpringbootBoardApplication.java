b5f03d0fe7421f1e41508f94463973069d453ad2
bmcho <bmcho91@naver.com>
2022-09-27 00:01:40 +0900

    #3 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    
    * 스프링 웹
    * 액츄에이터
    * 데브 툴즈
    * 롬복

---
+++ src/main/java/com/fastcampus/projectboard/ProjectSpringbootBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringbootBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSpringbootBoardApplication.class, args);
+    }
+
+}
